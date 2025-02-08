23c7c660e19e2e9163435c9b1ae9749a0d614d5b
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2025-02-08 16:44:07 +0530

    create qr gnerator screen

--- qr_scanner/lib/home_screen.dart
+++ qr_scanner/lib/home_screen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(24),
+              child: Text("QR CODE Max",
+              style:GoogleFonts.poppins(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+
+              ),
+
+              ),
+              
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(58),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    borderRadius: BorderRadius.circular(24),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 10,
+                        spreadRadius: 5,
+                      ),
+                    ]
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      _buildFeatureButton(
+                        context,
+                        "Generate QR Code",
+                        Icons.qr_code,
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GeneratorScreen()
+                          ))),
+                    ],
+
+                  )
+                ),
+                ),
+            ],
+          ),
+        ),
+    );
+
   }
 }
