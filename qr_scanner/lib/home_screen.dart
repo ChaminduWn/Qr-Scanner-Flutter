f7bd38da321173c30fd4fd663517717b7cb9094d
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2025-02-12 16:26:21 +0530

    Complete 100%

--- qr_scanner/lib/home_screen.dart
+++ qr_scanner/lib/home_screen.dart
@@ -9,105 +9,99 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 114, 7, 43),
+      backgroundColor: const Color.fromARGB(255, 61, 53, 53),
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.all(24),
-              child: Text("WN QR CODE",
-              style:GoogleFonts.poppins(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-
+            Center(
+              child: Text(
+                "WN QR CODE",
+                style: GoogleFonts.poppins(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-
-              ),
-              
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Container(
-                  padding: EdgeInsets.all(58),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.surface,
-                    borderRadius: BorderRadius.circular(24),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 10,
-                        spreadRadius: 5,
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(58),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(24),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    _buildFeatureButton(
+                      context,
+                      "Generate QR Code",
+                      Icons.qr_code,
+                      () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const GeneratorScreen(),
+                        ),
                       ),
-                    ]
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      _buildFeatureButton(
+                    ),
+                    const SizedBox(height: 20),
+                    _buildFeatureButton(
+                      context,
+                      "Scan QR Code",
+                      Icons.qr_code_scanner,
+                      () => Navigator.push(
                         context,
-                        "Generate QR Code",
-                        Icons.qr_code,
-                        () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => 
-                            const GeneratorScreen()),),),
-
-                     SizedBox(height: 20),                      
-                     _buildFeatureButton(
-                        context,
-                        "Scan QR Code",
-                        Icons.qr_code_scanner,
-                        () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => 
-                            const ScannerScreen()),),),
-
-                    ],
-
-                  )
+                        MaterialPageRoute(
+                          builder: (context) => const ScannerScreen(),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                ),
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
-
   }
+
   Widget _buildFeatureButton(BuildContext context, String title, IconData icon, VoidCallback onPressed) {
     return GestureDetector(
-      onTap: onPressed,      
+      onTap: onPressed,
       child: Container(
-        padding: EdgeInsets.all(15),
+        padding: const EdgeInsets.all(15),
         height: 200,
-        width:250,
-        decoration :BoxDecoration(
+        width: 240,
+        decoration: BoxDecoration(
           color: const Color.fromARGB(255, 114, 7, 43),
           borderRadius: BorderRadius.circular(15),
-
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
           children: [
             Icon(icon, size: 48, color: Colors.white),
             Text(
               title,
               style: GoogleFonts.poppins(
-                fontSize: 24,
+                fontSize: 20,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
               ),
             ),
-
           ],
         ),
-
       ),
-    );   
+    );
   }
 }
